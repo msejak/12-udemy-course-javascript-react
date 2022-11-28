a1d81933b7dd56910e7a2a989a5807ae327e7f34
kirsan <7o.msejak@gmail.com>
2022-11-28 20:53:08 +0300

    codingExercise3

--- script.js
+++ script.js
@@ -148,11 +148,104 @@
 // }
 // while (num <55);
 
-for (let i = 1; i < 10; i++) {
-  if (i === 6) {
-    // break;
-    continue;
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     // break;
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 2) break first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+// Место для первой задачи
+function firstTask() {
+  // Пишем решение вот тут
+  let num = 5;
+  while (num < 11) {
+    console.log(num);
+    num++;
+  }
+}
+
+// Место для второй задачи
+function secondTask() {
+  // Пишем решение вот тут
+  for (let i = 20; i > 9; i--) {
+    console.log(i);
+    if (i === 14) {
+      break;
+    }
+  }
+}
+
+// Место для третьей задачи
+function thirdTask() {
+  // Пишем решение вот тут
+  for (let i = 2; i < 11; i++) {
+    if (i % 2 !== 0) {
+      continue;
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+function fourthTask() {
+  // Пишем решение вот тут
+  let num = 3;
+  while (num < 16) {
+    console.log(num);
+    num = num + 2;
+  }
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  // Пишем решение вот тут
+  for (let i = 5; i < 11; i++) {
+    arrayOfNumbers.push(i);
   }
 
-  console.log(i);
+  // Не трогаем
+  return arrayOfNumbers;
 }
