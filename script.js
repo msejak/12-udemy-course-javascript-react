91addbc834f9d5a165c30c14a00ed24688715f02
kirsan <7o.msejak@gmail.com>
2022-11-28 16:26:34 +0300

    lesson-22

--- script.js
+++ script.js
@@ -26,8 +26,8 @@
 // 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 // 'Сколько фильмов вы уже посмотрели?'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
 
 // 2) Создать объект personalMovieDB и в него поместить такие свойства:
 //     - count - сюда передается ответ на первый вопрос
@@ -36,13 +36,13 @@ console.log(numberOfFilms);
 //     - genres - сюда поместить пустой массив
 //     - privat - в это свойство поместить boolean(логическое) значение false
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
-};
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
+// };
 
 // 3) Задайте пользователю по два раза вопросы:
 //     - 'Один из последних просмотренных фильмов?'
@@ -53,15 +53,15 @@ let personalMovieDB = {
 //         'logan': '8.1'
 //     }
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-  b = +prompt('На сколько оцените его?', ''),
-  c = prompt('Один из просмотренных фильмов?', ''),
-  d = +prompt('На сколько оцените его?', '');
+// const a = prompt('Один из просмотренных фильмов?', ''),
+//   b = +prompt('На сколько оцените его?', ''),
+//   c = prompt('Один из просмотренных фильмов?', ''),
+//   d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 // if (4 == 4) {
 //   console.log('Ok!');
@@ -84,3 +84,75 @@ console.log(personalMovieDB);
 //   default:
 //     console.log('В другой раз');
 // }
+
+// console.log( NaN || 2 || undefined );
+
+// console.log( NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( !1 && 2 || !3 );
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log('Done!');
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   // выводит 0, затем 1, затем 2
+//   alert(i);
+//   i++;
+// }
+
+// let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <55);
+
+for (let i = 1; i < 10; i++) {
+  if (i === 6) {
+    // break;
+    continue;
+  }
+
+  console.log(i);
+}
