b8692242c11a103b0e5f84e70c97dcae74638d92
kirsan <7o.msejak@gmail.com>
2022-11-29 19:58:07 +0300

    lesson-28

---
+++ 28-script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const logg = 'Hello world';
+
+console.log(logg.length);
+
+console.log(logg.toUpperCase());
+
+console.log(logg.toLowerCase());
+
+console.log(logg.indexOf('w'));
+
+console.log(logg.slice(6, 11));
