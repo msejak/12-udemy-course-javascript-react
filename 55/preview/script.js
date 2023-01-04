9dab41940f5b8d34ceab8098d449180ef981fdb4
kirsan <7o.msejak@gmail.com>
2023-01-04 21:47:13 +0300

    lesson-58

--- 55/preview/script.js
+++ 55/preview/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const boxesQuery = document.querySelectorAll('.box'),
+    boxesGet = document.getElementsByClassName('box');
+
+boxesQuery.forEach((box) => {
+    if (box.matches('.this')) {
+        console.log(box);
+    }
+});
+
+console.log(boxesQuery[0].closest('.wrapper'));
+
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// for (let i = 0; i < 5; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('box');
+//     document.body.append(div);
+// }
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// // console.log(document.body.children);
+
+// console.log(Array.from(boxesQuery));
+// console.log(Array.from(boxesGet));
