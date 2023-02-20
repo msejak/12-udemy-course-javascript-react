9c8fcdad44123df3c8cb062c2765700e18ea7a31
msejak <7o.mseJak@gmail.com>
2023-02-20 14:43:01 +0300

    lesson 123, 124

--- 122/preview/my-app/src/index.js
+++ 122/preview/my-app/src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const text = 'Hello World!';
+// const text = 'Hello World!';
 
-const elem = (
-    <div>
-        <h2 className="text">Текст: {text}</h2>
-        <input type="text" />
-        <label htmlFor=""></label>
-        <button tabIndex={0}>Click</button>
-    </div>
-);
+// const elem = (
+//     <div>
+//         <h2 className="text">Текст: {text}</h2>
+//         <input type="text" />
+//         <label htmlFor=""></label>
+//         <button tabIndex={0}>Click</button>
+//     </div>
+// );
 
 // const elem = React.createElement('h2', { className: 'greetings' }, 'Hello World!');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    elem
-    // <React.StrictMode>
-    //     <App />
-    // </React.StrictMode>
+    // elem
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
