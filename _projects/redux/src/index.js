5459fb91ae6296b01fda52be9be3f704a48dbadd
msejak <7o.mseJak@gmail.com>
2024-02-29 12:23:38 +0300

    feat: lesson 193

--- _projects/redux/src/index.js
+++ _projects/redux/src/index.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-import * as actions from './actions';
-// import App from './App';
 
-import './index.css';
-
-// let state = reducer(initialState, {type: 'INC'});
-// console.log('state 🚀', state);
+import App from './components/App';
 
 const store = createStore(reducer);
-
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-  console.log('state 🚀', store.getState());
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* {<App />} */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
